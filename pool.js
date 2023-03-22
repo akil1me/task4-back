4a730ae6b8fd3e4be6f16dd66e18484b923ba2fc
akil1me <akipax123@gmail.com>
2023-03-22 18:51:16 +0500

    add

--- pool.js
+++ pool.js
@@ -1,29 +1,14 @@
-// require("dotenv").config();
-// // const mysql = require("mysql");
-// // const connection = mysql.createConnection({
-// //   host: process.env.HOST_NAME,
-// //   user: process.env.USER_NAME,
-// //   password: process.env.PASSWORD,
-// //   database: process.env.DB_NAME,
-// //   port: 3306,
-// // });
-// // module.exports = connection;
-
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  user: "akillme",
-  host: "dpg-cgddjot269v52g6vk9u0-a.singapore-postgres.render.com",
-  database: "dbusers",
-  password: "drzNSmdGzQCB9m0jA9iZJMT07v7RoI7b",
+  user: "akobir",
+  host: "dpg-cgdg8bl269v52g7nok10-a.oregon-postgres.render.com",
+  database: "akobirdb",
+  password: "GYUYH9hE89UTSBCkaHljG57BNqfCqcIW",
   port: 5432,
-});
-pool.query("SELECT * FROM users", (err, res) => {
-  if (err) {
-    console.error("Error executing query", err.stack);
-  } else {
-    console.log("Connected to database at", res.rows[0].now);
-  }
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 module.exports = pool;
